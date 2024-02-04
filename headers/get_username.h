60a303e703157d3a8a6ba9941ee6b291119e5458
A-J-Sarmah <ajsarmah@tutanota.com>
2024-02-04 10:11:00 +0530

    Created basic structure of the project

---
+++ headers/get_username.h
@@ -0,0 +1,4 @@
+#ifndef __GET_USERNAME__
+#define __GET_USERNAME__
+
+#endif
---
+++ main.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include "./headers/get_username.h"
+
+int main(){
+    printf("Hello World \n");
+    return 0;
+}
